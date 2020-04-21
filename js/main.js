a9029548d289e401c11b2f62921d7b70fa93a257
Miloš Delibašić <63953317+milosdelibasic@users.noreply.github.com>
2020-04-21 13:04:57 +0200

    Uploading the website

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const responsive = {
+  0: {
+    items: 1,
+  },
+  320: {
+    items: 1,
+  },
+  560: {
+    items: 2,
+  },
+  960: {
+    items: 3,
+  },
+};
+
+$(document).ready(function () {
+  $nav = $(".nav");
+  $toggleCollapse = $(".toggle-collapse");
+
+  /* CLICK EVENT on toggle menu */
+  $toggleCollapse.click(function () {
+    $nav.toggleClass("collapse");
+  });
+
+  /* Owl-Carousel for Blog */
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    dots: false,
+    nav: true,
+    navText: [
+      $(".owl-navigation .owl-nav-prev"),
+      $(".owl-navigation .owl-nav-next"),
+    ],
+    responsive: responsive,
+  });
+
+  /* CLick to scroll top */
+  $(".move-up span").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      700
+    );
+  });
+
+  /* AOS Instansnce */
+  AOS.init();
+});
